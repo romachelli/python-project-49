de67ab924e93321e4ccd74aef5cb5c6989ef5b8f
romachelli <romakarabas63@gmail.com>
2023-01-24 19:52:17 +0400

    add brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-
+from brain_games.scripts.cli import welcome_user
 def greet(where):
     print(f'Welcome to the {where}!')
 def main():
     greet('Brain Games')
+    welcome_user()
 if __name__ == '__main__':
     main()

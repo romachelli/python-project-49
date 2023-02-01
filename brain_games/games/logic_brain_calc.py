d02571abe73b5adca682f0e9b735bd17a924a16d
romachelli <romakarabas63@gmail.com>
2023-02-01 22:42:54 +0400

    update Makefile

---
+++ brain_games/games/logic_brain_calc.py
@@ -0,0 +1,16 @@
+import random
+
+
+def game_rule():
+    print('What is the result of the expression?')
+
+
+def question_and_correct_answer():
+
+    first_operand = random.choice(range(5,60))
+    operator = random.choice(['+','-','*'])
+    second_operand = random.choice(range(5,30))
+    question = f'{first_operand}{operator}{second_operand}'
+    correct_answer = str(eval(question))
+
+    return question, correct_answer

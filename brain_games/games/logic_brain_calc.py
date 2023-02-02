e8e23fd81f5859d4f554a52f4a84524b75061e19
romachelli <romakarabas63@gmail.com>
2023-02-03 00:35:04 +0400

    update brain_games/games/logic_brain_calc.py

--- brain_games/games/logic_brain_calc.py
+++ brain_games/games/logic_brain_calc.py
@@ -10,7 +10,7 @@ def question_and_correct_answer():
     first_operand = random.choice(range(5,60))
     operator = random.choice(['+','-','*'])
     second_operand = random.choice(range(5,30))
-    question = f'{first_operand}{operator}{second_operand}'
+    question = f'{first_operand} {operator} {second_operand}'
     correct_answer = str(eval(question))
 
     return question, correct_answer
